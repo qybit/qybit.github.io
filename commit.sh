9e51ec22f7053224dc20379852989a727d1a12e9
qybit <2601970213@qq.com>
2021-01-26 23:47:38 +0800

    auto commit on

---
+++ commit.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+start_time=`date --date='0 days ago' "+%Y-%m-%d %H:%M:%S"`
+echo "hugo 打包文件中...."
+
+# 先进入 my_blog 博客源代码目录
+cd ..
+cd my_blog/
+
+# 开始提交源代码
+cur_time= `date "+%Y-%m-%d %H:%M:%S"`
+git add .
+git commit -m "auto commit on ${cur_time}"
+git push
+# 源码保存完毕
+echo "源码保存完毕"
+
+# 删除所有内容
+rm -rf public/*
+
+# 编译
+hugo
+
+# 将 public 下所有文件 全部转移到 qybit下
+mv ./public/* ../qybit/
+
+cd ..
+cd qybit/
+
+pwd
+
+git add .
+git commit -m "auto commit on ${cur_time}"
+git push
+
+finish_time=`date --date='0 days ago' "+%Y-%m-%d %H:%M:%S"`
+duration=$(($(($(date +%s -d "$finish_time")-$(date +%s -d "$start_time")))))
+echo "共计耗时：${duration}秒"
+
+pwd
