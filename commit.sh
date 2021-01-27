a2c43be90f9cb3ca32aefdc1dca08af1a0aa7622
qybit <2601970213@qq.com>
2021-01-27 14:40:02 +0800

    auto commit on

--- commit.sh
+++ commit.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+# 博客发布项目仓库 和 博客源码项目必须在同级目录
+
 start_time=`date --date='0 days ago' "+%Y-%m-%d %H:%M:%S"`
 echo "hugo 打包文件中...."
+
+# 删除 除 .git目录和commit.sh 之外的所有文件和文件夹
+rmm -rf `ls | egrep -v '(.git|commit.sh)'`
 
 # 先进入 my_blog 博客源代码目录
 cd ..
 cd my_blog/
 
 # 开始提交源代码
-cur_time= `date "+%Y-%m-%d %H:%M:%S"`
+cur_time= time=$(date "+%Y-%m-%d %H:%M:%S")
 git add .
 git commit -m "auto commit on ${cur_time}"
 git push
@@ -27,7 +32,6 @@ mv ./public/* ../qybit/
 cd ..
 cd qybit/
 
-pwd
 
 git add .
 git commit -m "auto commit on ${cur_time}"
@@ -36,5 +40,3 @@ git push
 finish_time=`date --date='0 days ago' "+%Y-%m-%d %H:%M:%S"`
 duration=$(($(($(date +%s -d "$finish_time")-$(date +%s -d "$start_time")))))
 echo "共计耗时：${duration}秒"
-
-pwd
